b1c56313eaec8232f1cd4148ed79aa9622192ca5
james nee <james@james>
2014-08-18 21:07:54 -0700

    Ensure the right data is queried

--- FlyOlinFly/views.py
+++ FlyOlinFly/views.py
@@ -67,7 +67,7 @@ def content():
 	cur = db_session.execute('select fname, lname, phonenum, email, flightdesc, datetime, comment, sorter from entry order by datetime')
 	
 	#Delete all entries that are more than 30 days old
-	print db_session.query(Entry).filter(datetime.now() > (Entry.datetime + timedelta(days=30)))
+	print db_session.query(Entry).filter(datetime.now() > (Entry.datetime + timedelta(days=30)))[0][5]
 
 	giver_rows = []
 	entries_rows = []
