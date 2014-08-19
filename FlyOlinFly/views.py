a567a1d5d0179aa63ba2f503578e7a3e242de180
james nee <james@james>
2014-08-18 20:30:51 -0700

    Fingers Crossed

--- FlyOlinFly/views.py
+++ FlyOlinFly/views.py
@@ -72,7 +72,8 @@ def content():
 		creation_arrival = row[5]
 		expiration = creation_arrival + timedelta(days = 30)
 		if datetime.now() > expiration:
-			print "DONE"
+			db.session.delete(row)
+			db.session.commit()
 		if row[7] == "offering":
 			giver_rows.append(row)
 		else:
